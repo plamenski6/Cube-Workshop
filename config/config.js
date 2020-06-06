8f87f5852f779a98b63e4f10e48ea3fd01129176
Plamen Shumanski <plamen1030@abv.bg>
2020-06-06 14:13:26 +0300

    feat: connect with db

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseURL:`mongodb+srv://plamenski6:dbPass@cluster0-j6rki.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
